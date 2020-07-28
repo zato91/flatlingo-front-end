12769e65f2397b6ace005d7906601e33b90b9ccf
andy-aguilar <aaguil3@gmail.com>
2020-07-28 17:59:52 -0400

    finished card flip and started add deck modal

--- javascript/main.js
+++ javascript/main.js
@@ -4,29 +4,35 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
 
 
-    // // Handling card flipping
+    // Handling card flipping
     const cardFronts = document.querySelectorAll(".flip-card-front");
     const cardBacks = document.querySelectorAll(".flip-card-back");
+    
 
     cardFronts.forEach(FlipFunction)
     cardBacks.forEach(BackFlipFunction)
 
+    // CREATE DECK MODAL ISH
+    const createDeckButton = document.getElementById("create-deck-button");
+    const createDeckModal = document.getElementById("create-deck-modal");
+    const closeSpan = document.getElementsByClassName("close")[0];
 
+    createDeckButton.addEventListener("click", (e)=>{
+        createDeckModal.style.display = "block";
+    })
 
-        // document.addEventListener("click", (e) => {
-        //     if (e.target.className == "flip-card-front"){
-        //         e.target.parentElement";
-        //     }
-        //     else if (e.target.className == "flip-card-back"){
-        //         e.target.parentElement.style.transform = ""
-        //     }
-        //     else {
-        //         console.log(e.target)
-        //     }
-            
-        // })
+    closeSpan.addEventListener("click", (e)=>{
+        createDeckModal.style.display = "none";
+    })
+    document.addEventListener("click", (e)=>{
+        if(e.target == createDeckModal){
+            createDeckModal.style.display = "none";
+        }
+    })
 })
 
+
+// CardFlipFunctions
 const FlipFunction = (cardFront) => {
     cardFront.addEventListener("click", (e) => {
         //e.target.parentElement.style.transform = "rotateY(180deg)";
@@ -36,5 +42,6 @@ const FlipFunction = (cardFront) => {
 const BackFlipFunction = (cardBack) => {
     cardBack.addEventListener("click", (e) => {
         e.target.closest(".flip-card-inner").style.transform = "";
+        
     })
 }
