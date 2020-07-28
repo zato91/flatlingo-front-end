6895d3bcaa7419cddb6e2e5bbfd885ec8443a031
andy-aguilar <aaguil3@gmail.com>
2020-07-28 16:23:35 -0400

    done moving card flip functionality to JS

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,37 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
 
 
-    // Handling card flipping
+    // // Handling card flipping
+    const cardFronts = document.querySelectorAll(".flip-card-front");
+    const cardBacks = document.querySelectorAll(".flip-card-back");
+
+    cardFronts.forEach(FlipFunction)
+    cardBacks.forEach(BackFlipFunction)
 
 
+
+        // document.addEventListener("click", (e) => {
+        //     if (e.target.className == "flip-card-front"){
+        //         e.target.parentElement";
+        //     }
+        //     else if (e.target.className == "flip-card-back"){
+        //         e.target.parentElement.style.transform = ""
+        //     }
+        //     else {
+        //         console.log(e.target)
+        //     }
+            
+        // })
 })
+
+const FlipFunction = (cardFront) => {
+    cardFront.addEventListener("click", (e) => {
+        //e.target.parentElement.style.transform = "rotateY(180deg)";
+        e.target.closest(".flip-card-inner").style.transform = "rotateY(180deg)";
+    })
+}
+const BackFlipFunction = (cardBack) => {
+    cardBack.addEventListener("click", (e) => {
+        e.target.closest(".flip-card-inner").style.transform = "";
+    })
+}
