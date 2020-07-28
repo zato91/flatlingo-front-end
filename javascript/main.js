e80133d28d4d3eebd4c4b3523817599e244451b7
zato91 <zatokey@gmail.com>
2020-07-28 17:04:24 -0400

    adding js Fetch creating div deck

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,29 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     // Handling Login and Links
+    const url = 'http://localhost:3000/users/1'
+
+    function fetchUser(){
+        fetch(url)
+            .then(response => response.json())
+            .then(user => user.decks.forEach(deck => { renderDeck(deck) }));
+
+    }
+
+    function renderDeck(deck){
+        
+        let divDeckContainer = document.getElementById('deck-container')
+
+        let divDeck = document.createElement('div');
+        divDeck.classList += 'button';
+        divDeck.innerText = deck.name;
+        divDeckContainer.append(divDeck);
+
+    }
 
 
 
 
+    fetchUser();
     // Handling card flipping
 
 
